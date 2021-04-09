0f51ec65db60b049a629a6290336eaa3c32bec1b
MichaelHamonangan <hamonanganmichael@mail.ugm.ac.id>
2021-04-09 19:29:14 +0700

    Add project files.

---
+++ User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTS_460552
+{
+    abstract class User
+    {
+        public string username;
+        public abstract void Welcome();
+        public abstract string Exit();
+    }
+}
